29012d4d8a478ea13db82eaa1a30acd4c80ae25d
Joe Dai <dm.joedai@gmail.com>
2024-02-11 11:23:12 -0500

    Add constants #defines

---
+++ include/pins.hpp
@@ -0,0 +1,19 @@
+#ifndef __PINS_H
+#define __PINS_H
+
+
+#define PIN_BT_RX       2
+#define PIN_BT_TX       3
+
+#define PIN_MOTOR_L     10
+#define PIN_MOTOR_R     11
+
+#define PIN_IR_DATA     4
+
+#define PIN_SERVO       5
+
+#define PIN_ULTRASONIC_TRIG     6
+#define PIN_ULTRASONIC_ECHO     7
+
+
+#endif  // __PINS_H
---
+++ include/settings.hpp
@@ -0,0 +1,9 @@
+#ifndef __SETTINGS_H
+#define __SETTINGS_H
+
+
+#define DEBUG           true
+#define BT_BAUDRATE     9600
+
+
+#endif  // __SETTINGS_H
