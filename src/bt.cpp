cc9bf4f6184b5cebab54acbb0f65c253a9f9fbcd
Lune <lunasun918@gmail.com>
2024-02-11 12:31:43 -0500

    uploaded bt tut module

--- src/bt.cpp
+++ src/bt.cpp
@@ -1,13 +1,345 @@
+#include "bt.hpp"
+
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-#include "bt.hpp"
+/* Bluetooth Interfacing with Arduino to drive a rover.
 
+
+   This code allows users onwindows devices to controll their robots.
+
+   joystick values: abcdefg
+
+   abc are from 0-360 and represent the angle in cartesian coordinates.
+   def are from 0-100 and represent how far the stick is from the center
+   position. g is the push button with 0 indicating no button pressed and int
+   between 1 and 4 indicating that buttons 1-4 are pressed.
+
+   Windows:
+
+   Bellow is linked the app that you must use to controll your robot.
+   https://apps.microsoft.com/store/detail/universal-bluetooth-pad/9NBLGGH2PL8B
+   It has the same functionallity as the other app only includes an additional
+   slider which can be usefull for servo controll.
+
+   A quirk of the Windows app include that the channel wont be sent untill
+   either the joystick, or slider have been moved. It is also important to
+   select both the "Text" and "Channel" boxes (found on the right hand side)
+   before data can be sent.
+
+   ***IMPORTANT NOTES***: When using the Windows app you must give it '/n' as
+   its input. You must also unplug the the bluetooth modules TX and RX wires
+   from the arduino when uploading code since it wont upload with the bluetooth
+   module plugged in (power and ground hoever can stay connected).
+
+*/
+
+//_________________________________________________Your Input Goes
+//Below____________________________________________________________________________
+// Here the pin values used for motor control are established.
+
+int DIR_L_F = 13;  // Left Direction pin that will indicate forewards movement
+                   // (1 for forewards, 0 for backwards).
+int DIR_L_B = 12;  // Left Direction pin that will indicate backwards movement
+                   // (1 for backwards, 0 for forewards).
+int PWM_L = 11;    // Speed controll pin. *** This pin must be plugged into an
+                   // output pin on the arduino that is labled PWM ***.
+
+int DIR_R_F = 4;  // Right Direction pin that will indicate forewards movement
+                  // (1 for forewards, 0 for backwards)
+int DIR_R_B = 2;  // Left Direction pin that will indicate backwards movement (1
+                  // for backwards, 0 for forewards).
+int PWM_R = 3;    // Speed controll pin. *** This pin must be plugged into an
+                  // output pin on the arduino that is labled PWM ***.
+
+//__________________________________________________Your Input Goes
+//Above____________________________________________________________________________
+
+void GetBTCommand();  // Outputs a list containing the raw controller inputs.
+                      // This function is given a list pointer, and a character
+                      // teling it which app is being used.
+void BluetoothPadTranslateCommand();  // Gets used by GetBTCommand() when '\n'
+                                      // is sent as an input. This function
+                                      // decodes the string message and fills
+                                      // the "TranslatedCommand" list.
+void SimpleMapInput();  // A pre write function That takes in TransatedCommand
+                        // as an imput. It uses the Angle and Power inputs
+                        // (first 2 indexes of the input list) and output both
+                        // motor speed and direction information for the left
+                        // and right motors.
+void ExecuteCommand_L298N();  // Takes in a 4 element list (Left Direction,
+                              // Right Direction, Left Speed, Right Speed) and
+                              // applies the correct outputs to the 6 pins used
+                              // to drive the motors. (these pins are determined
+                              // by you at the begining of the code)
 BT::BT(int rx_pin, int tx_pin) {
-    _rx_pin = rx_pin;
-    _tx_pin = tx_pin;
+  _rx_pin = rx_pin;
+  _tx_pin = tx_pin;
 }
 
-void BT::init() {
-    
+float ControllerInput[8];  // Angle, speed, slider value, channel 0-3 (Windows)
+float MotorOutputs[4];     // Left Direction, Right Direction, Left Speed, Right
+                           // Speed.
+
+// The setup function variables can be given initial values (although you can do
+// that when declaring the variable too) and where pins are given their desired
+// type. This function only runs once at the begining and can should be where
+// you declare what pins will be used for things like servos and sensors.
+void setup() {
+  Serial.begin(9600);  // Initializing the Serial Port to Baud rate 9600. You
+                       // can change this to be faster if you wish.
+
+  // Setting up pins:
+  pinMode(DIR_L_F, OUTPUT);
+  pinMode(DIR_L_B, OUTPUT);
+  pinMode(PWM_L, OUTPUT);
+
+  pinMode(DIR_R_F, OUTPUT);
+  pinMode(DIR_R_B, OUTPUT);
+  pinMode(PWM_R, OUTPUT);
 }
+
+//___________________________________________________YOUR CODE GOES
+//BELOW___________________________________________________________________________
+
+void loop() {
+  GetBTCommand('\n', ControllerInput);  // '\n' for Windows
+  SimpleMapInput(MotorOutputs, ControllerInput);
+  ExecuteCommand_L298N(MotorOutputs);
+}
+
+//____________________________________________________YOUR CODE GOES
+//ABOVE____________________________________________________________________________
+/*
+  Using the variables "ControllerInput" and the '\n' (Windows) characters,
+  GetBTCommand() should be called first to get the raw controller inputs. The 2
+  conficurations you will use are:
+
+  GetBTCommand('\n', ControllerInput); (Windows)
+
+  This fuction bassically fills the "ControllerImput" list. This list has 8
+  indexes split up in the followig way:
+
+  Windows: [Joystick Angle, Joystick Distcance from Center, Slider Value,
+  Channel 0, Channel 1, Channel 2, Channel 3]
+
+  This list is then used to decide the motor speeds and directions. You have the
+  option of using the pre writen function SimpleMapInput() which takes in the
+  previously filled list "ControllerInput", and the return list "MotorOutput".
+  The function uses the first 2 elements of "ControllerInput" and maps them to
+  directio and PWM values for the left and right motors. these 4 values are
+  placed in the "MotorOutput" list. An implimentation of this function and a
+  breakdown of the output can be seen below:
+
+  SimpleMapInput(MotorOutputs, ControllerInput);
+
+  MotorOutputs = [Left Motor Direction, Right Motor Direction, Left Motor Speed,
+  Right Motor Speed]
+
+  If you want the extra challange or want slightly different behaviours, you can
+  write your own version of this function but make sure all your hardware is
+  working correctly beforehand to avoid complex debugging.
+
+  Once your output has been calculated it is time to apply these states to the
+  pins. This is done by the ExecuteCommand_L298N() function which takes in the
+  "MotorOutputs" list. using the speed and direction values stored in the imput
+  list, this function applies the desired outputs to the 4 direction pins and 2
+  PWM pins. Make sure that you dont change the names of these pins when
+  declaring them because the ExecuteCommand_L298N() function references them
+  directly.
+
+  This series of events must happen every loop and if done correctly you should
+  be able to drive your rover after writing only 3 lines of code!!!
+
+  Note that the buttons and or sliders are free for you to use to add additional
+  functionality such as controlling servos and switching between manual control,
+  autonomous control and standby modes.
+
+*/
+
+void GetBTCommand(char border, float* ans) {
+  char data;            // Variable to store the data
+  String Command = "";  // variable to be returned
+  while (true) {
+    if (Serial.available()) {  // Checks the availability of Serial port
+      data = Serial.read();    // Read the data and stores it in variable.
+      if (data == border) {    // Different commands are separated by # or \n
+                             // characters. If you read in a # or \n it is clear
+                             // that a command has just ended.
+        if (border == '#') {
+          UnicaTranslateCommand(ans, Command);
+          return;
+        } else {
+          BluetoothPadTranslateCommand(ans, Command);
+          return;
+        }
+      } else {
+        Command =
+            Command +
+            String(
+                data);  // Append the character to the end of the command string
+      }
+    }
+  }
+}
+
+void BluetoothPadTranslateCommand(float* ans, String Input) {
+  int X;
+  int Y;
+  int S;
+  int C = Input.substring(0, 1).toInt();
+  int start = 2;
+  int count = 2;
+  int i = 0;
+
+  while (i < 2) {
+    count++;
+
+    if (Input.substring(count, count + 1) == ",") {
+      if (i == 0) {
+        X = Input.substring(start, count).toInt();
+        start = count + 1;
+        i++;
+      } else if (i == 1) {
+        Y = Input.substring(start, count).toInt();
+        start = count + 1;
+        i++;
+        S = Input.substring(start, Input.length()).toInt();
+      }
+    }
+  }
+
+  float angle = 180.0 * atan2(Y, X) / 3.14;
+  if (Y < 0) {
+    angle += 360.0;
+  }
+
+  float power = sqrt((X * X) + (Y * Y));
+  if (power > 100) {
+    power = 100.0;
+  }
+  power = power * 2.55;
+
+  ans[0] = angle;
+  ans[1] = power;
+  ans[2] = S;
+  for (int i = 3; i < 7; i++) {
+    if ((C + 3) == i) {
+      ans[i] = 1;
+    } else {
+      ans[i] = 0;
+    }
+  }
+}
+
+void UnicaTranslateCommand(float* ans, String Input) {
+  float angle = float(
+      Input.substring(0, 3).toInt());  // First get the abc numbers out of the
+                                       // command and then convert to integer.
+  float power = float(
+      Input.substring(3, 6).toInt() *
+      2.55);  // Next get the def numbers out of the command and then convert to
+              // integer. Finally convert this 0-100 value into a 0-255 value.
+  int button = float(
+      Input.substring(6).toInt());  // Finally get the g number out of the
+                                    // command and then convert to integer.
+  ans[0] = angle;
+  ans[1] = power;
+  for (int i = 2; i < 7; i++) {
+    if (button == (i - 1)) {
+      ans[i] = 1;
+    } else {
+      ans[i] = 0;
+    }
+  }
+
+  Serial.println(ans[0]);
+  Serial.println(ans[1]);
+  Serial.println(ans[2]);
+  Serial.println(ans[3]);
+  Serial.println(ans[4]);
+  Serial.println(ans[5]);
+  Serial.println(ans[6]);
+  Serial.println("");
+}
+
+void SimpleMapInput(float* ans, float* Input) {
+  float angle = Input[0];
+  float power = Input[1];
+  int L_direction;
+  int R_direction;
+  float L_speed;
+  float R_speed;
+
+  // define direction for L motor (corrasponds with motor A on the motor driver)
+  if (angle <= 180 || angle >= 330) {
+    L_direction = 1;
+  } else {
+    L_direction = 0;
+  }
+
+  // define direction for R motor (corrasponds with motor B on the motor driver)
+  if (angle <= 200) {
+    R_direction = 1;
+  } else {
+    R_direction = 0;
+  }
+
+  // define L speed
+  if (angle >= 90 && angle <= 270) {
+    L_speed =
+        abs(((angle - 90.0) / 90.0) - 1.0) *
+        power;  // The angle will range from 90 -> 270 and will output a L_Speed
+                // range of 1 -> -1, multiplied by the the power variable.
+  } else if (angle < 90) {
+    L_speed =
+        (0.5 + (angle / 180.0)) *
+        power;  // The angle will range from 0 -> 90 and will output a L_Speed
+                // range of 0.5 -> 1, multiplied by the the power variable.
+  } else {
+    L_speed = abs(0.7 + (((angle - 360.0) / 90.0) * 1.7)) *
+              power;  // The angle will range from 270 -> 360 and will output a
+                      // L_Speed range of -1 -> 0.5, multiplied by the the power
+                      // variable.
+  }
+
+  // define R speed
+  if (angle <= 90) {
+    R_speed = (angle / 90.0) * power;
+  } else if (angle >= 270) {
+    R_speed = abs((angle - 360) / 90) * power;
+  } else if (angle > 90 && angle < 180) {
+    R_speed = (1 - ((angle - 90) / 180)) * power;
+  } else {
+    R_speed = abs(0.7 - (((angle - 180.0) / 90.0) * 1.7)) * power;
+  }
+
+  ans[0] = (L_direction);
+  ans[1] = (R_direction);
+  ans[2] = (L_speed);
+  ans[3] = (R_speed);
+}
+
+void ExecuteCommand_L298N(float* Command) {
+  if (Command[0] > 0) {
+    digitalWrite(DIR_L_F, HIGH);
+    digitalWrite(DIR_L_B, LOW);
+  } else {
+    digitalWrite(DIR_L_B, HIGH);
+    digitalWrite(DIR_L_F, LOW);
+  }
+
+  if (Command[1] > 0) {
+    digitalWrite(DIR_R_F, HIGH);
+    digitalWrite(DIR_R_B, LOW);
+  } else {
+    digitalWrite(DIR_R_B, HIGH);
+    digitalWrite(DIR_R_F, LOW);
+  }
+
+  // Next set speed:
+  analogWrite(PWM_L, Command[2]);
+  analogWrite(PWM_R, Command[3]);
+}
+
+void BT::init() {}
