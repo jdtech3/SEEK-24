9b05fde82e027be84fc5fbcb79630a0170d9ce76
Joe Dai <dm.joedai@gmail.com>
2024-02-11 11:39:39 -0500

    Update #defines

--- include/pins.hpp
+++ include/pins.hpp
@@ -5,15 +5,17 @@
 #define PIN_BT_RX       2
 #define PIN_BT_TX       3
 
-#define PIN_MOTOR_L     10
-#define PIN_MOTOR_R     11
+#define PIN_MOTOR_L_IN1 6
+#define PIN_MOTOR_L_IN2 9
+#define PIN_MOTOR_R_IN1 10
+#define PIN_MOTOR_R_IN2 11
 
 #define PIN_IR_DATA     4
 
 #define PIN_SERVO       5
 
-#define PIN_ULTRASONIC_TRIG     6
-#define PIN_ULTRASONIC_ECHO     7
+#define PIN_ULTRASONIC_TRIG     7
+#define PIN_ULTRASONIC_ECHO     8
 
 
 #endif  // __PINS_H
--- include/settings.hpp
+++ include/settings.hpp
@@ -5,5 +5,8 @@
 #define DEBUG           true
 #define BT_BAUDRATE     9600
 
+#define MOTOR_L_DIR     true
+#define MOTOR_R_DIR     false
+
 
 #endif  // __SETTINGS_H
